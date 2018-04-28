1ada1f874162ad975fb06d06f68bc962eff05050
Keith Bennett <kanezor@gmail.com>
2018-04-28 00:49:15 -0500

    add a test for listening with no name selected

--- koti-tcp/test/net_listener_test_local.cpp
+++ koti-tcp/test/net_listener_test_local.cpp
@@ -5,4 +5,28 @@
 
 namespace koti {
 
+TYPED_TEST_P(net_listener_tests, listen_no_name_selected)
+{
+	auto & listener_ = this->remake();
+
+	EXPECT_NO_THROW(listener_->listen());
+	EXPECT_TRUE(listener_->is_open());
+	EXPECT_TRUE(listener_->is_bound());
+	EXPECT_TRUE(listener_->is_listening());
+	EXPECT_FALSE(listener_->had_listener_error_);
+	EXPECT_EQ(listener_->last_listener_error_, boost::system::error_code());
+	EXPECT_EQ(listener_->last_listener_error_.message(), boost::system::error_code().message());
+	EXPECT_FALSE(listener_->local_endpoint().path().empty());
+}
+
+REGISTER_TYPED_TEST_CASE_P(
+	net_listener_tests,
+		listen_no_name_selected
+);
+INSTANTIATE_TYPED_TEST_CASE_P(
+	local,
+	net_listener_tests,
+	net_listener_local_tests
+);
+
 } // namespace koti
